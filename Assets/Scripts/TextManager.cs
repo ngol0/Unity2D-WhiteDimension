a614e7c7499201df554593f3245486978a3b7b58
lampngo <44044782+ngol0@users.noreply.github.com>
2022-12-19 11:50:42 -0800

    organize folder

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TextManager : MonoBehaviour
 {
-   
+   [SerializeField] TextSO textData;
+
+   void ShowText()
+   {
+
+   }
+
+   public void OnNextText()
+   {
+
+   }
 }
--- Assets/Scripts/TextSO.cs
+++ Assets/Scripts/TextSO.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class TextEntry
+{
+    public int index;
+    [TextArea(3,10)] public string textEntry;
+}
+
 [CreateAssetMenu(menuName = "Text SO")]
 public class TextSO : ScriptableObject
 {
-    
+    public List<TextEntry> textEntries;
 }
